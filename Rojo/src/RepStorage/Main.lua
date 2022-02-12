737af4ffe61ce5fac5d2610e6e57ffe36204bb69
Andrew <76942911+alexfeed1990@users.noreply.github.com>
2022-02-12 17:32:39 +0200

    uh

--- Rojo/src/RepStorage/Main.lua
+++ Rojo/src/RepStorage/Main.lua
@@ -1,2 +1,2 @@
 local HttpService = game:GetService("HttpService")
-local script = loadstring(HttpService:GetAsync("https://github.com/test/test/test.lua", true))
+local script = loadstring(HttpService:GetAsync("https://raw.githubusercontent.com/alexfeed1990/Roblo2DX/main/Main.lua", true))
